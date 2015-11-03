fea05f1bc7fc5d7a97d71a35c6d300219c3decd4
Simon Jupp <simon.jupp@gmail.com>
2015-11-03 23:27:25 +0000

    changed default version URI to owl:versionInfo

--- ontology-tools/src/main/java/uk/ac/ebi/spot/ols/config/OntologyDefaults.java
+++ ontology-tools/src/main/java/uk/ac/ebi/spot/ols/config/OntologyDefaults.java
@@ -12,6 +12,6 @@ public class OntologyDefaults {
     public static String DEFINITION = "http://purl.org/dc/elements/1.1/description";
     public static String CREATOR = "http://purl.org/dc/terms/1.1/creator";
     public static String MAILINGLIST = "http://usefulinc.com/ns/doap#mailing-list";
-    public static String VERSION = "http://purl.org/pav/version";
+    public static String VERSION = "http://www.w3.org/2002/07/owl#versionInfo";
     public static String HOMEPAGE = "http://xmlns.com/foaf/0.1/homepage";
 }
