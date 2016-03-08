6e105cd1ef8d55ad623db8496d0abee12e566fa5
Simon Jupp <simon.jupp@gmail.com>
2016-03-08 14:37:01 +0100

    added suggest to home page

--- ols-web/src/main/resources/static/js/ols-termselect.js
+++ ols-web/src/main/resources/static/js/ols-termselect.js
@@ -60,4 +60,42 @@ $(document).ready(function() {
         });
 
     });
+
+    $( "input[data-olswidget='suggest']" ).each(function() {
+
+        var relativePath = $(this).data("selectpath") ? $(this).data("selectpath") : '';
+        var ontology =   $(this).data("olsontology") ? $(this).data("olsontology") : '';
+        $(this).devbridgeAutocomplete({
+            serviceUrl: relativePath + 'api/suggest',
+            minChars: 3,
+            dataType : 'json',
+            paramName: 'q',
+            params: {ontology : ontology},
+            //onSelect : function (suggestion)  {
+            //    var type = getUrlType(suggestion.data.type);
+            //    var encoded = encodeURIComponent(suggestion.data.iri);
+            //    window.location.href = relativePath + 'ontologies/' + suggestion.data.ontology + "/" + type + '?iri=' + encoded;
+            //},
+            transformResult: function(response) {
+                return {
+                    suggestions: $.map(response.response.docs, function(dataItem) {
+                        return { value: dataItem.autosuggest};
+                    })
+                };
+            }
+            //formatResult: function (suggestion, currentValue) {
+            //
+            //    var label = suggestion.data.label ;
+            //    var extra = "";
+            //    if (suggestion.data.synonym != "") {
+            //        label =  suggestion.data.synonym;
+            //        extra = "<div class='sub-text'>synonym for " + suggestion.value + "</div>"
+            //    }
+            //
+            //    return "<div style='width: 100%; display: table;'> <div style='display: table-row'><div  style='display: table-cell;' class='ontology-suggest'><div class='suggestion-value'>" + label + "</div>" + extra + "</div><div style='vertical-align:middle; text-align: right; width:60px; display: table-cell;'><div class='ontology-source'>" + suggestion.data.prefix + "</div></div></div></div>";
+            //}
+        });
+
+    });
+
 });
