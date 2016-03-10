c71c38959315937e9d00b91d0adfa712a6081cbd
Simon Jupp <simon.jupp@gmail.com>
2016-03-10 11:12:44 +0100

    playing with new typeahead

---
+++ ols-web/src/main/resources/static/js/ols-termselect2.js
@@ -0,0 +1,17 @@
+// constructs the suggestion engine
+var states = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.whitespace,
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  // `states` is an array of state names defined in "The Basics"
+  local: ['test1', 'test2', 'test3']
+});
+
+$('#bloodhound .typeahead').typeahead({
+  hint: true,
+  highlight: true,
+  minLength: 1
+},
+{
+  name: 'states',
+  source: states
+});
