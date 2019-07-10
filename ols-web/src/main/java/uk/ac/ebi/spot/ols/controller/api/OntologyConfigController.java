d959816244e0fbba27e18eb49cfc58f15c9c5d66
Simon Jupp <simon.jupp@gmail.com>
2019-07-10 15:56:21 +0100

    added an API endpoint at api/ols-config that returns config in yaml. You can also filter on ontology ids using the ids parameter fixes #275

--- ols-web/src/main/java/uk/ac/ebi/spot/ols/controller/api/OntologyConfigController.java
+++ ols-web/src/main/java/uk/ac/ebi/spot/ols/controller/api/OntologyConfigController.java
@@ -1,50 +1,33 @@
 package uk.ac.ebi.spot.ols.controller.api;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.rest.webmvc.RepositoryLinksResource;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.hateoas.*;
-import org.springframework.hateoas.mvc.ControllerLinkBuilder;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriUtils;
-import uk.ac.ebi.spot.ols.exception.ErrorMessage;
+import uk.ac.ebi.spot.ols.config.OntologyResourceConfig;
 import uk.ac.ebi.spot.ols.model.OntologyDocument;
-import uk.ac.ebi.spot.ols.neo4j.model.Term;
-import uk.ac.ebi.spot.ols.neo4j.service.OntologyTermGraphService;
 import uk.ac.ebi.spot.ols.service.OntologyRepositoryService;
-import uk.ac.ebi.spot.ols.util.OLSEnv;
+import uk.ac.ebi.spot.ols.util.ReasonerType;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.UnsupportedEncodingException;
-import java.util.Map;
+import java.net.URI;
+import java.util.*;
 
 /**
  * @author Simon Jupp
- * @date 19/08/2015
+ * @date 10/07/2019
  * Samples, Phenotypes and Ontologies Team, EMBL-EBI
  */
-@Controller
-@RequestMapping("/api/ontologies")
-@ExposesResourceFor(OntologyDocument.class)
-public class OntologyController implements
-        ResourceProcessor<RepositoryLinksResource> {
+@RestController
+@RequestMapping("/api/ols-config")
+public class OntologyConfigController {
 
     private Logger log = LoggerFactory.getLogger(getClass());
 
@@ -55,39 +38,160 @@ public class OntologyController implements
     @Autowired
     private OntologyRepositoryService ontologyRepositoryService;
 
-    @Autowired DocumentAssembler documentAssembler;
 
-    @Autowired TermAssembler termAssembler;
+    @RequestMapping(path = "", produces = {"text/yaml"}, method = RequestMethod.GET)
+    String getOntologies(
+            @RequestParam(value = "ids", required = false, defaultValue = "") Collection<String> ids
+    ) {
 
-    @Override
-    public RepositoryLinksResource process(RepositoryLinksResource resource) {
-        resource.add(ControllerLinkBuilder.linkTo(OntologyController.class).withRel("ontologies"));
-        return resource;
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+
+        List<OntologyResourceConfig> configs = new ArrayList<OntologyResourceConfig>();
+        for (OntologyDocument document : ontologyRepositoryService.getAllDocuments()) {
+
+            if (ids.isEmpty()) {
+                OntologyResourceConfigFormatter ex = new OntologyResourceConfigFormatter(document.getConfig());
+                configs.add(ex);
+            }
+            else if (ids.contains(document.getOntologyId())) {
+                OntologyResourceConfigFormatter ex = new OntologyResourceConfigFormatter(document.getConfig());
+                configs.add(ex);
+            }
+        }
+
+        try {
+            return mapper.writeValueAsString(configs);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+
     }
 
-    @RequestMapping(path = "", produces = {MediaType.APPLICATION_JSON_VALUE, MediaTypes.HAL_JSON_VALUE}, method = RequestMethod.GET)
-    HttpEntity<PagedResources<OntologyDocument>> getOntologies(
-            @PageableDefault(size = 20, page = 0) Pageable pageable,
-            PagedResourcesAssembler assembler
-    ) throws ResourceNotFoundException {
-        Page<OntologyDocument> document = ontologyRepositoryService.getAllDocuments(pageable);
-        return new ResponseEntity<>( assembler.toResource(document, documentAssembler), HttpStatus.OK);
-    }
-
-
-    @RequestMapping(path = "/{onto}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaTypes.HAL_JSON_VALUE}, method = RequestMethod.GET)
-    HttpEntity<Resource<OntologyDocument>> getOntology(@PathVariable("onto") String ontologyId) throws ResourceNotFoundException {
-        ontologyId = ontologyId.toLowerCase();
-        OntologyDocument document = ontologyRepositoryService.get(ontologyId);
-        if (document == null) throw new ResourceNotFoundException();
-        return new ResponseEntity<>( documentAssembler.toResource(document), HttpStatus.OK);
-    }
 
     @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Resource not found")
     @ExceptionHandler(ResourceNotFoundException.class)
     public void handleError(HttpServletRequest req, Exception exception) {
     }
 
+    private class OntologyResourceConfigFormatter extends OntologyResourceConfig {
+
+        private OntologyResourceConfigFormatter(OntologyResourceConfig config) {
+            this.setId(config.getId());
+            this.setVersionIri(config.getVersionIri());
+            this.setTitle(config.getTitle());
+            this.setNamespace(config.getNamespace());
+            this.setPreferredPrefix(config.getPreferredPrefix());
+            this.setDescription(config.getDescription());
+            this.setHomepage(config.getHomepage());
+            this.setMailingList(config.getMailingList());
+            this.setFileLocation(config.getFileLocation());
+            this.setReasonerType(config.getReasonerType());
+            this.setLabelProperty(config.getLabelProperty());
+            this.setDefinitionProperties(config.getDefinitionProperties());
+            this.setSynonymProperties(config.getSynonymProperties());
+            this.setHierarchicalProperties(config.getHierarchicalProperties());
+            this.setBaseUris(config.getBaseUris());
+        }
+
+        @Override
+        @JsonIgnore
+        public String getVersionIri() {
+            return super.getVersionIri();
+        }
+
+        @Override
+        @JsonIgnore
+        public String getVersion() {
+            return super.getVersion();
+        }
+
+        @Override
+        @JsonIgnore
+        public Collection<String> getCreators() {
+            return super.getCreators();
+        }
+
+        @Override
+        @JsonIgnore
+        public Collection<String> getInternalMetadataProperties() {
+            return super.getInternalMetadataProperties();
+        }
+
+        @Override
+        @JsonIgnore
+        public Collection<URI> getHiddenProperties() {
+            return super.getHiddenProperties();
+        }
+
+        @Override
+        @JsonIgnore
+        public boolean isSkos() {
+            return super.isSkos();
+        }
+
+        @Override
+        @JsonProperty("uri")
+        public String getId() {
+            return super.getId();
+        }
+
+        @Override
+        @JsonProperty("id")
+        public String getNamespace() {
+            return super.getNamespace();
+        }
+
+        @Override
+        @JsonProperty("ontology_purl")
+        public URI getFileLocation() {
+            return super.getFileLocation();
+        }
+
+        @Override
+        @JsonProperty("mailing_list")
+        public String getMailingList() {
+            return super.getMailingList();
+        }
+
+        @Override
+        @JsonProperty("label_property")
+        public URI getLabelProperty() {
+            return super.getLabelProperty();
+        }
+
+        @Override
+        @JsonProperty("synonym_property")
+        public Collection<URI> getSynonymProperties() {
+            return super.getSynonymProperties();
+        }
+
+        @Override
+        @JsonProperty("definition_property")
+        public Collection<URI> getDefinitionProperties() {
+            return super.getDefinitionProperties();
+        }
+
+        @Override
+        @JsonProperty("base_uri")
+        public Collection<String> getBaseUris() {
+            return super.getBaseUris();
+        }
+
+        @Override
+        @JsonProperty("hierarchical_property")
+        public Collection<URI> getHierarchicalProperties() {
+            return super.getHierarchicalProperties();
+        }
+
+        @Override
+        @JsonProperty("reasoner")
+        public ReasonerType getReasonerType() {
+            return super.getReasonerType();
+        }
+    }
 
 
 }
+
