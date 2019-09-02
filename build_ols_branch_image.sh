e4f17f3c761f53c95e90a651238a6cf0259d377e
Warren Read <wread@ebi.ac.uk>
2019-09-02 14:27:46 +0100

    Improve and ease runnability of OLS branches within Docker
    Make it possible to extract, build and run dockerised OLS not only by
    branch, but by individual Git commit. Add entrypoint script to run
    services (specifically MongoDB and Solr, but optionally incuding Tomcat,
    and by extension OLS itself) automatically. All services currently run
    within a single container, without orchestration. Multiple versions of
    OLS can nonetheless be run simulataneously, within different containers,
    using different port mappings to expose service endpoints: to effect
    this, an integer increment to the standard port numbers can simply be
    passed to the run script as its final parameter; use different increment
    values if calling the script more than once.
    
    Add entrypoint script, volume delete script, supervisor config file and
    optional addendum thereto (for Tomcat).

--- build_ols_branch_image.sh
+++ build_ols_branch_image.sh
@@ -2,5 +2,18 @@
 
 if [ -z "$1" ]; then exit 1; fi
 branch=$1
+commit=$2
+IFS=":${IFS}"
 
-docker build --build-arg ols_branch=${branch} -f OlsBranchDockerfile -t ols-${branch}-debian .
+echo "ols-${*}"
+
+#  Might want to try _not_ passing the second parameter 'commit' if it
+#  is not actually supplied. It seems to break the build if set empty,
+#  whereas if not handed on at all, the associated dockerfile should
+#  just default to 'HEAD'.
+
+DOCKER_BUILDKIT=1
+docker build --build-arg ols_branch=${branch} \
+             --build-arg ols_commit=${commit} \
+             -f OlsBranchDockerfile \
+             -t "ols-branch-${*}" .
--- run_ols_branch_image.sh
+++ run_ols_branch_image.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-#  Initialise OLS container; maybe change/increment mongo and solr port nos. (so
-#  as not to interfere with local-OS-native equivalents, if running).
+#  Initialise OLS container and change/increment MongoDB and Solr port
+#  nos. (so as not to interfere e.g. with local-OS-native equivalents,
+#  if running). If you don't want to specify a commit id, ensure that
+#  you enter parameter 2 as the empty string (""), and the run should
+#  default to the latest image having the name part corresponding to
+#  parameter 1. You need to enter a value or at least the empty string
+#  for parameter 2, because parameter 3 (integer increment for port
+#  nos.) is _required_; without it this script will exit on error.
+#
+#  Parameter 4 is "tomcat" if you have already loaded the OLS data and
+#  are ready to run the full OLS web application. Otherwise leave blank.
 
-if [ -z "$2" ]; then exit 1; fi
+[ -z "$3" ] && exit 1
 branch=$1
-port_inc=$2
+commit=$2
+port_inc=$3
+entrypoint_p1=$4
 
 HOST_TOMCAT_PORT=$(( 8080 + port_inc ))
 HOST_SOLR_PORT=$(( 8983 + port_inc ))
 HOST_MONGO_PORT=$(( 27017 + port_inc ))
 
+#  Admittedly, these functions don't do the pop themselves, not yet anyway
+pop_and_join_volume () { local IFS="_"; echo "${*}"; }
+named_volume="`pop_and_join_volume ${branch} ${commit}`"
+pop_and_join_image () { local IFS=":"; echo "${*}"; }
+named_image="`pop_and_join_image ${branch} ${commit}`"
+
 docker run -it \
            -p ${HOST_TOMCAT_PORT}:8080 \
            -p ${HOST_SOLR_PORT}:8983 \
            -p ${HOST_MONGO_PORT}:27017 \
-           -v ols_${branch}_mongo_data:/data/db \
-	   -v ols_${branch}_autosuggest:/home/ols/.ols/solr/autosuggest \
-           -v ols_${branch}_ontology:/home/ols/.ols/solr/ontology \
-           ols-${branch}-debian bash
+           -v ols_${named_volume}_mongo_data:/data/db \
+           -v ols_${named_volume}_neo_data:/home/ols/.ols/neo4j \
+           -v ols_${named_volume}_autosuggest:/home/ols/.ols/solr/autosuggest \
+           -v ols_${named_volume}_ontology:/home/ols/.ols/solr/ontology \
+           ols-branch-${named_image} ${entrypoint_p1}
