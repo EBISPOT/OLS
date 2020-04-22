8eeda16f4660aca9bada2228e3ceda635400c282
James McLaughlin <james@mclgh.net>
2020-04-22 14:21:39 +0100

    New UI tweaks, including collapsible sections

--- ols-web/src/main/resources/static/js/ols.js
+++ ols-web/src/main/resources/static/js/ols.js
@@ -23,4 +23,34 @@ $(function() {
     $('p.annotation-value').each(function(i, el) {
         $(el).html($(el).html().replace(/((http|https|ftp):\/\/[^\s,]+)/g, "<a href=\"$1\">$1</a>"))
     })
+
+    $('.panel-heading').each(function(i, heading) {
+        var open = true
+        $(heading).children('h4').append(
+            $('<div/>').text('-').addClass('toggle').click(function() {
+                if(open) {
+                    $(heading).siblings('.panel-body').hide()
+                    open = false
+                    $(this).text('+')
+                } else {
+                    $(heading).siblings('.panel-body').show()
+                    open = true
+                    $(this).text('-')
+                }
+            })
+        )
+    })
+
+    var treeButtonsOpen = true
+    $('.ols-tree-buttons').prepend(
+        $('<div/>').text('-').addClass('toggle').click(function() {
+            if(treeButtonsOpen) {
+                $(this).text('+').siblings().hide()
+                treeButtonsOpen = false
+            } else {
+                $(this).text('-').siblings().show()
+                treeButtonsOpen = true
+            }
+        })
+    )
 })
