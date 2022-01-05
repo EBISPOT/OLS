85ed1af99b1f2c7f884a2593597f50e25e322e65
Josh Lagrimas <josh.lagrimas@gmail.com>
2022-01-05 23:10:28 +0800

    issue-506-cp: align Individuals view with Properties and Terms vies
    initial fix for #506

--- ols-web/src/main/java/uk/ac/ebi/spot/ols/controller/ui/OntologyControllerUI.java
+++ ols-web/src/main/java/uk/ac/ebi/spot/ols/controller/ui/OntologyControllerUI.java
@@ -3,13 +3,22 @@ package uk.ac.ebi.spot.ols.controller.ui;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import uk.ac.ebi.spot.ols.model.OntologyDocument;
+import uk.ac.ebi.spot.ols.neo4j.model.Individual;
+import uk.ac.ebi.spot.ols.neo4j.service.OntologyIndividualService;
 import uk.ac.ebi.spot.ols.neo4j.service.OntologyTermGraphService;
 import uk.ac.ebi.spot.ols.service.OntologyRepositoryService;
 import uk.ac.ebi.spot.ols.util.OLSEnv;
@@ -39,6 +48,9 @@ public class OntologyControllerUI {
     private OntologyTermGraphService ontologyTermGraphService;
 
     @Autowired
+    private OntologyIndividualService ontologyIndividualService;
+
+    @Autowired
     private CustomisationProperties customisationProperties;
 
     // Reading these from application.properties
@@ -56,6 +68,7 @@ public class OntologyControllerUI {
     @RequestMapping(path = "/{onto}", method = RequestMethod.GET)
     String getTerm(
             @PathVariable("onto") String ontologyId,
+            @PageableDefault(size = Integer.MAX_VALUE, page = 0) Pageable pageable,
             Model model) throws ResourceNotFoundException {
 
         ontologyId = ontologyId.toLowerCase();
@@ -70,32 +83,39 @@ public class OntologyControllerUI {
                 InternetAddress address = new InternetAddress(contact, true);
                 contact = "mailto:" + contact;
             } catch (Exception e) {
-              // only thrown if not valid e-mail, so contact must be URL of some sort
+                // only thrown if not valid e-mail, so contact must be URL of some sort
             }
-            model.addAttribute("contact", contact);
 
+            if (pageable.getSort() == null) {
+                pageable = new PageRequest(pageable.getPageNumber(), pageable.getPageSize(), new Sort(new Sort.Order(Sort.Direction.ASC, "n.label")));
+            }
+            Page<Individual> individuals = ontologyIndividualService.findAllByOntology(ontologyId, pageable);
+
+            model.addAttribute("contact", contact);
             model.addAttribute("ontologyDocument", document);
+            model.addAttribute("ontologyIndividuals", individuals);
 
             customisationProperties.setCustomisationModelAttributes(model);
             DisplayUtils.setPreferredRootTermsModelAttributes(ontologyId, document, ontologyTermGraphService, model);
-        }
-        else {
+        } else {
             return homeController.doSearch(
                     "*",
                     null,
-                    null,null, null, false, null, false, false, null, 10,0,model);
+                    null, null, null, false, null, false, false, null, 10, 0, model);
         }
         return "ontology";
     }
 
     @RequestMapping(path = "/{onto}", produces = "application/rdf+xml", method = RequestMethod.GET)
-    public @ResponseBody FileSystemResource getOntologyDirectDownload(@PathVariable("onto") String ontologyId, HttpServletResponse response) throws ResourceNotFoundException {
+    public @ResponseBody
+    FileSystemResource getOntologyDirectDownload(@PathVariable("onto") String ontologyId, HttpServletResponse response) throws ResourceNotFoundException {
         return getDownloadOntology(ontologyId, response);
     }
 
 
     @RequestMapping(path = "/{onto}/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE, method = RequestMethod.GET)
-    public @ResponseBody  FileSystemResource getDownloadOntology(@PathVariable("onto") String ontologyId, HttpServletResponse response) throws ResourceNotFoundException {
+    public @ResponseBody
+    FileSystemResource getDownloadOntology(@PathVariable("onto") String ontologyId, HttpServletResponse response) throws ResourceNotFoundException {
 
         ontologyId = ontologyId.toLowerCase();
 
@@ -105,12 +125,12 @@ public class OntologyControllerUI {
             throw new ResourceNotFoundException("Ontology called " + ontologyId + " not found");
         }
 
-        if(document.getConfig().getAllowDownload() == false) {
+        if (document.getConfig().getAllowDownload() == false) {
             throw new ResourceNotFoundException("This ontology is not available for download");
         }
 
         try {
-            response.setHeader( "Content-Disposition", "filename=" + ontologyId + ".owl" );
+            response.setHeader("Content-Disposition", "filename=" + ontologyId + ".owl");
             return new FileSystemResource(getDownloadFile(ontologyId));
         } catch (FileNotFoundException e) {
             throw new ResourceNotFoundException("This ontology is not available for download");
@@ -118,8 +138,8 @@ public class OntologyControllerUI {
     }
 
 
-    private File getDownloadFile (String ontologyId) throws FileNotFoundException {
-        File file = new File (getDownloadsFolder(), ontologyId.toLowerCase());
+    private File getDownloadFile(String ontologyId) throws FileNotFoundException {
+        File file = new File(getDownloadsFolder(), ontologyId.toLowerCase());
         if (!file.exists()) {
             throw new FileNotFoundException();
         }
@@ -127,7 +147,7 @@ public class OntologyControllerUI {
     }
 
 
-    private String getDownloadsFolder ( ) {
+    private String getDownloadsFolder() {
         if (downloadsFolder.equals("")) {
             return OLSEnv.getOLSHome() + File.separator + "downloads";
         }
